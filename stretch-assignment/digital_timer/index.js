47ce98c99ab64f360077f64bbc1faa3d2707e8b1
Henry Blevins <heblevi@gmail.com>
2019-03-04 18:44:09 -0500

    Added fields to specify seconds and timer format
    The code to do this is very, very ugly because I ran out of time.
    
    I also know minute don't work correctly, because seconds roll over at 100
    instead of 60. The whole digitobj nonsense just needs to be refactored to handle
    that.

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -28,8 +28,67 @@ let timerLock;
 function startTimer() {
     if (!timerLock) {
         document.querySelectorAll(".timer .digit").forEach(e => e.classList.remove("finished"));
-        timerLock = makeTimer(0, 10, 10000, {"msTens": 1, "msHundreds": 2, "secondOnes": 3, "secondTens": 4});
+        let length = document.getElementById("length").value * 1000;
+        let digitobj = parseFormat(document.getElementById("format").value);
+        timerLock = makeTimer(0, 10, length, digitobj);
+    } else {
+        document.querySelector(".timer button").classList.add("shake");
+        setTimeout(() => document.querySelector(".timer button").classList.remove("shake"), 1000);
     }
+}
+
+function parseFormat(formatstr) {
+    let digits = document.querySelector(".digits");
+    while (digits.firstChild) digits.removeChild(digits.firstChild);
+    let digitobj = {};
+    formatstr.split("").forEach(function (c) {
+        if (c === "s") {
+            let tens = document.createElement("div");
+            tens.classList.add("digit");
+            tens.id = "secondTens";
+            tens.textContent = "-";
+            digitobj["secondTens"] = 4;
+            digits.appendChild(tens);
+            let ones = document.createElement("div");
+            ones.classList.add("digit");
+            ones.id = "secondOnes";
+            ones.textContent = "-";
+            digitobj["secondOnes"] = 3;
+            digits.appendChild(ones);
+        } else if (c === "u") {
+            let tens = document.createElement("div");
+            tens.classList.add("digit");
+            tens.id = "msHundreds";
+            tens.textContent = "-";
+            digits.appendChild(tens);
+            digitobj["msHundreds"] = 2;
+            let ones = document.createElement("div");
+            ones.classList.add("digit");
+            ones.id = "msTens";
+            ones.textContent = "-";
+            digitobj["msTens"] = 1;
+            digits.appendChild(ones);
+        } else if (c == "m") {
+            let tens = document.createElement("div");
+            tens.classList.add("digit");
+            tens.id = "minuteTens";
+            tens.textContent = "-";
+            digits.appendChild(tens);
+            digitobj["minuteTens"] = 6;
+            let ones = document.createElement("div");
+            ones.classList.add("digit");
+            ones.id = "minuteOnes";
+            ones.textContent = "-";
+            digitobj["minuteOnes"] = 5;
+            digits.appendChild(ones);
+        } else {
+            let elem = document.createElement("div");
+            elem.classList.add("digit");
+            elem.textContent = c;
+            digits.appendChild(elem);
+        }
+    });
+    return digitobj;
 }
 
 // window.onload = () => makeTimer(10000, -10, 0, {"msTens": 1, "msHundreds": 2, "secondOnes": 3, "secondTens": 4});
