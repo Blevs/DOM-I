b2946164e2b2c8954cb9653f9c1348604c17e8a8
Henry Blevins <heblevi@gmail.com>
2019-03-04 18:00:17 -0500

    Added button with functionality and styled page

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -13,12 +13,23 @@ function makeTimer(start, step, stop, digitsobj) {
     let timer = setInterval(function() {
         ms += step;
         if (ms === stop) {
-            digits.forEach((pair) => pair[0].style.color = "red");
+            digits.forEach((pair) => pair[0].classList.add("finished"));
+            new Audio('3d_ping.wav').play();
             clearInterval(timer);
+            timerLock = null;
         }
         setDigits(ms);
     }, Math.abs(step));
     return timer;
 }
 
-window.onload = () => makeTimer(10000, -10, 0, {"msTens": 1, "msHundreds": 2, "secondOnes": 3, "secondTens": 4});
+let timerLock;
+
+function startTimer() {
+    if (!timerLock) {
+        document.querySelectorAll(".timer .digit").forEach(e => e.classList.remove("finished"));
+        timerLock = makeTimer(0, 10, 10000, {"msTens": 1, "msHundreds": 2, "secondOnes": 3, "secondTens": 4});
+    }
+}
+
+// window.onload = () => makeTimer(10000, -10, 0, {"msTens": 1, "msHundreds": 2, "secondOnes": 3, "secondTens": 4});
